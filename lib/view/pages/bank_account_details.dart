e667383cceae2f6163d6ce9db6601690ef7e52d0
michael <michaelsameh8@gmail.com>
2024-04-22 21:04:45 +0200

    routing first line..

--- lib/view/pages/bank_account_details.dart
+++ lib/view/pages/bank_account_details.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
 import 'package:voo_app/view/pages/login_screen.dart';
+import 'package:voo_app/view/pages/we_will_review_screen.dart';
 import 'package:voo_app/view/widgets/main_elevated_button.dart';
 
 class BankAccountDetailsScreen extends StatelessWidget {
@@ -120,7 +121,7 @@ class BankAccountDetailsScreen extends StatelessWidget {
                   SizedBox(height: 10.h,),
                   MainElevatedButton(
                       nextScreen:
-                      LoginScreen(),
+                      WeWillReviewScreen(),
                       text: 'Next',
                       backgroundColor: Color(0xff0038A7)),
                   SizedBox(height: 4.h,),

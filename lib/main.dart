71bdea920723b9e7994f7eca03c7af17ea648367
Mark <markadel111222@gmail.com>
2024-05-25 23:33:43 +0400

    Merging

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
 import 'package:voo_app/Controller/Data/data_cubit.dart';
+import 'package:voo_app/firebase_options.dart';
 import 'package:voo_app/view/pages/splash_screen.dart';
 import 'Controller/Login/login_cubit.dart';
 import 'Controller/dio-helper.dart';
@@ -16,6 +18,7 @@ void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   // Lama bt3ml init le 7agat fel main lazem tktb el line beta3 ensure Initialized dh
   DioHelper.init();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   // Bensha8al el DIO .. elly howa bngeb meno el data mel API
   CacheHelper.init();
   //  Bensha8al el shared prefrences .. 3ashan law fe remember me .. aw 7aga fe local storage
